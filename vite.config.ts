3d2a7720c1a7632bbb1426b51c29a73ee784701d
tacotinnn <humodevinfo@gmail.com>
2025-09-12 00:29:30 +0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Projenizin küçük ve basit olduğu için özel bir yapılandırmaya ihtiyacı yok.
+  // Bu temel dosya yeterlidir.
+})
